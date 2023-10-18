04bbec3ffd8601cc7ee03b08d1cc4b474029fb20
kiatklai <likhitthaworn.k@gmail.com>
2023-10-18 21:50:24 +0900

    サーバーから情報を取得し、画面に表示させる

--- productsApp/urls.py
+++ productsApp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from productsApp import views
 
 urlpatterns=[
-  path("",views.index)
+  path("",views.index),
+  path("product/details/<int:id>",views.productDetail,name="productDetail"),
+  path("products",views.products)
 ]
--- productsApp/views.py
+++ productsApp/views.py
@@ -4,3 +4,11 @@ from productsApp.models import Product
 def index(request):
   products = Product.objects.filter(isTrending=True)
   return render(request,"index.html",{"products":products})
+
+def productDetail(request,id):
+  product=Product.objects.get(pk=id)
+  return render(request,"detail.html",{"product":product})
+
+def products(request):
+  all_products = Product.objects.all()
+  return render(request,"products.html",{"all_products":all_products})
