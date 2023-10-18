8c97af2e259f6f66fc88b16e0e8d1197098566c5
kiatklai <likhitthaworn.k@gmail.com>
2023-10-18 22:12:58 +0900

    ページを分け、ページ番号を設定

--- productsApp/views.py
+++ productsApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from productsApp.models import Product
+from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
   products = Product.objects.filter(isTrending=True)
@@ -11,4 +12,8 @@ def productDetail(request,id):
 
 def products(request):
   all_products = Product.objects.all()
+  #page設定
+  page = request.GET.get("page")
+  paginator = Paginator(all_products,3)
+  all_products = paginator.get_page(page)
   return render(request,"products.html",{"all_products":all_products})
